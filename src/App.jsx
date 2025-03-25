bdccc225e461380ed1aba78581a5f416744e7d03
MaxiCacciamano <maximilianocacciamano@gmail.com>
2025-03-25 14:10:40 -0300

    tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css'
 import { Changepassword } from './assets/Comonent/Changepassword'
+import { Confirmacion } from './page/Confirmacion'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <Changepassword/>
-      </div>
-
-    </>
+    <BrowserRouter>
+     <Routes>
+        <Route path='/' element = {<Changepassword/>} />
+        <Route path="/next-page" element = {<Confirmacion />} />
+     </Routes>
+    </BrowserRouter>
   )
 }
 
