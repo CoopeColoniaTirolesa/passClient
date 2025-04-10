2d8f8a75245927453d9bc9779a4278ce5b694d16
MaxiCacciamano <maximilianocacciamano@gmail.com>
2025-04-10 14:23:23 -0300

    finish

--- src/assets/Comonent/Sello.jsx
+++ src/assets/Comonent/Sello.jsx
@@ -2,6 +2,6 @@
 export const  Sello = () => {
 
     return (
-        <div style={{textAlign:'center'}}>© 2025 Maximiliano Cacciamano</div>
+        <div style={{textAlign:'center', color:'white'}}><a style={{color:'white'}} href="https://maximilianodev.vercel.app">© 2025 created by Maximiliano Cacciamano</a></div>
     )
 }
